19cf376fa5cfdb9519e88dd623857ad72c6a096c
ajtelesnavarro <ajtelesnavarro@gmail.com>
2026-08-26 14:45:41 -0300

    feat: add Produtos page and CardProduto component
    - Created Produtos page to list products with details.
    - Added CardProduto component to display individual product information.
    - Included CSS file for CardProduto styling.

---
+++ src/app/produtos/page.jsx
@@ -0,0 +1,43 @@
+export default function Produtos(){
+
+    const listaProdutos = [
+        {
+            titulo: "Detegente YPÊ",
+            descricao: "É legal, mas nâo pode bebe :(",
+            imagem: "detergente.png",
+            perco: 5.87
+        },
+        {
+            titulo: "Sabão em Pó MINUAN",
+            descricao: "Mais área de cotnato, mais poder",
+            imagem: "sabao.png",
+            perco: 15.33
+        },
+        {
+            titulo: "Desinfetante MINUA",
+            descricao: "HMMMMM é cremoso, é saboroso... perai, errei!",
+            imagem: "desinfetante.png",
+            perco: 4.00
+        },
+        {
+            titulo: "Perfume PINHOSOL",
+            descricao: "Ué cade o pinho que eu ia cozinhar?",
+            imagem: "perfumePINHOSOL.png",
+            perco: 4.00
+        },
+        {
+            titulo: "Amaciante de NENEM",
+            descricao: "COM-PLE-TA-MEN-TE biodegradavel",
+            imagem: "amacianteNENEM.png",
+            perco: 4.00
+        }
+    ];
+
+    return(
+        <>
+            <h1>Página de listagem de produtos</h1>
+
+            {/* aqui card produto aqui */}
+        </>
+    )
+}
---
+++ src/components/cardProduto/index.jsx
@@ -0,0 +1,15 @@
+export default function CardProduto({
+    titulo,
+    descricao,
+    imagem,
+    preco
+}){
+    return(
+        <div className="card-wrapper">
+            <h2>{titulo}</h2>
+            <img src={"/imagens/produtos/" + imagem}/>
+            <p>{descricao}</p>
+            <p>Preço: R$ <b>{preco}</b></p>
+        </div>
+    )
+}
