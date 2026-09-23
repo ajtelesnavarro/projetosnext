8f2009536949976577a99a92d7a13445f65f374d
Ana Julia <ajtelesnavarro@gmail.com>
2026-09-23 17:09:29 -0300

    changes

---
+++ projeto02/projeto02/src/app/filmes/[id]/page.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import dados from "@/filmes.json";
+import { useState, useEffect } from "react";
+import "./idFilme.css";
+
+export default function Filme() {
+    const [filme, setFilme] = useState(null);
+    const params = useParams();
+
+    useEffect(() => {
+        const filmeEncontrado = dados.find(f => f.id == params.id)
+        setFilme(filmeEncontrado);
+    }, [])
+
+    return (
+        <main>
+            {filme != null && <div className="wrapper-filme">
+                <div className="info-geral">
+                    <img src={filme.imagem}></img>
+                </div>
+                <div className="info-especificacoes">
+                    <div className="filme-titulo">
+                        <div className="filme-titulo-principal">
+                            <h1>{filme.titulo}, "{filme.tituloOriginal}"</h1>
+                            {filme.diretores.length === 1 ? (
+                                <h2>Dirigido por <span className="filme-diretores">{filme.diretores[0]}</span></h2>
+                            ) : (
+                                <h2>Dirigido por <span className="filme-diretores">{filme.diretores.join(", ")}</span></h2>
+                            )}
+                        </div>
+                        <div className="filme-titulo-tags">
+                            {filme.generos.map((gen) => (
+                                <span key={gen} className="filme-titulo-tag">{gen}</span>
+                            ))}
+                            {filme.tags.map((tags) => (
+                                <span key={tags} className="filme-titulo-tag">{tags}</span>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="filme-detalhes">
+                        <h2>
+                            {filme.sinopse}
+                        </h2>
+                        <div className="filme-detalhes-elenco">
+                            <h2>
+                                {filme.elenco.map((ator) => (
+                                    <p key={ator.nome}>
+                                        {ator.nome} — {ator.personagem}
+                                    </p>
+                                ))}
+                            </h2>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>}
+        </main>
+    )
+}
--- projeto02/projeto02/src/components/CardFilme/index.jsx
+++ projeto02/projeto02/src/components/CardFilme/index.jsx
@@ -1,9 +1,16 @@
 import "./cardFilme.css";
 
-export default function CardFilme({filme}){
-    return(
+export default function CardFilme({ filme }) {
+    return (
         <div className="wrapper-filme">
-            <img src={filme.imagem} alt=""/>
+            <div className="info-geral">
+                <img src={filme.imagem} alt="" />
+                <p>{filme.titulo}</p>
+            </div>
+            <div className="info-especificacoes">
+                <p>{filme.sinopse}</p>
+                <a href={`/filmes/${filme.id}`}>Saiba mais!</a>
+            </div>
         </div>
     )
 }
