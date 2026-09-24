bd27aceceef2f69f450656a59784fbf81bfa659a
ajtelesnavarro <ajtelesnavarro@gmail.com>
2026-09-24 17:09:14 -0300

    more code

--- projetoprodutos/src/app/produtos/page.jsx
+++ projetoprodutos/src/app/produtos/page.jsx
@@ -24,8 +24,9 @@ export default function Produtos() {
             {listaProdutos.length > 0 ? 
                 <div className="card-produtos-grid">
                     {listaProdutos.map((idx)=>{
+                        
                         const slug = produto.title.toLowerCase();
-                        slug = slug.replace()
+                        slug = slug.replace(" ", "-")
                         return <CardProduto key={idx.id} produto={idx}/>
                     })}
                 </div>
